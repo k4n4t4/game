9aeacd56df2a2b2517544a1f9b6006b6f9cafe74
nimu <n_k>
2024-12-23 21:29:03 +0900

    update

--- game/sprites/player.js
+++ game/sprites/player.js
@@ -1,18 +1,46 @@
 import Sprite from "./../libs/sprite.js";
 
 export default class Player extends Sprite {
-  constructor(scene) {
+  x;
+  y;
+
+  constructor(scene, x, y) {
     super(scene);
     this.name = "player";
 
+    this.x = x;
+    this.y = y;
   }
 
   update() {
+    const game = this.game;
+    const key = game.key;
 
+    if (key.down("ArrowRight")) {
+      this.x++;
+    }
+    if (key.down("ArrowLeft")) {
+      this.x--;
+    }
+    if (key.down("ArrowUp")) {
+      this.y--;
+    }
+    if (key.down("ArrowDown")) {
+      this.y++;
+    }
+
+    if (key.down("q")) {
+      game.event.emit("scene", "title");
+    }
   }
 
   draw() {
     const ctx = this.scene.layer[0].context;
 
+    ctx.drawImage(
+      this.scene.assets.images.player,
+      0, 0, 16, 16,
+      this.x, this.y, 16, 16
+    );
   }
 }
